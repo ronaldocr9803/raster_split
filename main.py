2b3ecc390bca9bcecb25293c136e79f4f51823b5
root <ronaldocr9803@gmail.com>
2020-11-12 15:18:41 +0000

    add make folder code

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ from natsort import natsorted
 import glob
 
 if __name__ == "__main__":
+    if not os.path.exists('images'):
+        os.makedirs('images')
+    if not os.path.exists('labels'):
+        os.makedirs('labels')    
     lst_raster = ['W03_202003311249_RI_RSK_RSKA014702_RGB', 'W05_202003281250_RI_RSK_RSKA003603_RGB']
     for raster in lst_raster:
         print("Process {}...".format(raster))
-        lst_raster_img = natsorted([i for i in os.listdir(raster) if i[-4:]=='.tif'])
+        lst_raster_img = natsorted([i for i in os.listdir(os.path.join(".","data",raster)) if i[-4:]=='.tif'])
         for raster_img in lst_raster_img:
-            img_path = os.path.join(".", raster, raster_img)
-            shfPath = os.path.join(".", raster, "{}.shp".format(raster))
+            print("Process {}".format(raster_img))
+            img_path = os.path.join(".","data", raster, raster_img)
+            shfPath = os.path.join(".","data", raster, "{}.shp".format(raster))
             process_one_image(img_path, shfPath)
         # os.listdir(glob.glob(os.path.join(".",raster))[0])
         # 
